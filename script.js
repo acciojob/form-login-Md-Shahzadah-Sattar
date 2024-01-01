5a0bdb950dab15533690d9c59fa0a138162e7f4c
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-01 21:08:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function getFormvalue() {
-    //Write your code here
+// function getFormvalue() {
+//     //Write your code here
 
+// }
+// script.js
+
+function getFormvalue() {
+    // Get the values of first and last name from the form
+    var firstName = document.forms["form1"]["fname"].value;
+    var lastName = document.forms["form1"]["lname"].value;
+
+    // Display the values using alert
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+
+    // Return false to prevent the form from submitting (optional)
+    return false;
 }
